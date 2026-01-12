f15c14170fdf2c6c1e834e7d93c4e0db2c4adbb0
Pochao Wang <dark781228@gmail.com>
2026-01-12 23:38:54 +0800

    Updated the FROM source from stg_products to int_products_enriched to strengthen utilization of the Silver layer.

--- dbt_ecommerce/models/marts/core/dim_products.sql
+++ dbt_ecommerce/models/marts/core/dim_products.sql
@@ -1,3 +1,11 @@
 select
-    *
-from {{ ref('stg_products') }}
+    product_id,
+    category_name, 
+    name_length,
+    description_length,
+    photos_quantity,
+    weight_g,
+    length_cm,
+    height_cm,
+    width_cm
+from {{ ref('int_products_enriched') }}
